2aa1c7366b5a914c5f3342b6d799bb4c5bbe3597
giovannyortegon <ortegon.giovanny@hotmail.com>
2021-11-07 01:59:53 -0500

    [ADD] basic go, print, variables, input and output

---
+++ MasterGO/Chap01-GO-OS/curly.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+//{			// raise a error
+	fmt.Println("Go has strict rules for curly braces!!")
+}
---
+++ MasterGO/Chap01-GO-OS/stdIN.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var f *os.File
+
+	f = os.Stdin
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		fmt.Println(">", scanner.Text())
+	}
+}
